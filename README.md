0805cbec9e727579a1d76b3653744fb242e26c17
ReubenDev <reubenkiarie8m@gmail.com>
2023-04-21 12:23:15 +0300

    add FUNCTIONALLITY & STYLING

--- README.md
+++ README.md
@@ -5,3 +5,78 @@ This template should help get you started developing with Vue 3 in Vite. The tem
 ## Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+### **A NOTE-TAKING APP MADE BY VUE 3**
+
+In this application we will be able to write a note and add it to the list of notes.
+
+#### HOW THE NOTES APP WORKS
+
+We have a "plus" button that when pressed, opens the notes modal.To implement this functionality, we create a `showModal ref` variable to store the state of the modal which is a `boolean` value, so either `true` or `false`. By default its false so that we dont see it when the page loads.
+
+At the plus button, we add an event listener `@click` which will set the value of the `showModal` to `true` so that we can see our modal.
+
+```vue modal setup
+<script setup>
+import { ref } from "vue";
+const showModal = ref(false);
+</script>
+
+<template>
+  <button @click="showModal = true"></button>
+</template>
+```
+
+We have bound `showModal` value to the modal, so when we click the '+' button the value is turned `true` and the _modal_ appears. An example of what is happening behind the scenes is the following button that when clicked it disappears:
+
+```vue conditional rendering
+<script setup>
+//Import ref from vue
+const showHeading = ref(true);
+</script>
+<template>
+  <button
+    v-bind="showHeading"
+    @click="showHeading = !showHeading"
+    v-if="showHeading"
+  >
+    If TRUE, now you see me!
+  </button>
+</template>
+```
+
+As you can see, we are first binding the value of `showHeading` to the
+button so that the element can change according to the value of `showHeading`.
+In vue, elements are rendered conditionally, meaning if a value is `true` then
+it is rendered to the DOM otherwise if `falsy` it is removed from the DOM. We
+then attach an `event listener` which is `@click` which triggers the change of
+`showHeading` when clicked. Lastly we have a `v-if` which controlls the
+condition of `showHeading` value.
+
+Bind >> Listen for event >> Render if true.
+
+Now back to our notes app, if `showModal` is true, then we show the modal which is an overlay.
+
+##### **MODAL**
+
+The modal consists of a `textarea` , 'Add Note' & 'Close' button.
+
+The textarea is 2-way-bound to `newNote` variable which is an empty string. This means that when we type something in the textarea, the value of `newNote` changes.
+
+The 'Add Note' button adds the newNote to the list of notes we have.It has an event listener which triggers `addNote()` function. This function 'pushes' new note to a `notes` variable that is an empty array. addNote function pushes an object with a note's properties like `id, text, date, & backgroundColor`
+The text is initialized to newNote.value which is the input on the textbox.
+
+The 'Close Button' has an event listener that turns the value of `showModal` to `false`, hence closing the modal.
+
+In the modal we have `v-model.trim` which is to trim our input and get rid of whitespaces in the text. We can also trim in the if condition checking for the length of our text.
+`if(newNote.value.trim.length)`. But the prefered way is to do it in the v-model.
+
+We also have a warning message which appears when the user tries to add a list when the characters are below 10.
+This is implemented by having a `newNotesWarning` which is a ref of empty string. We conditionally render the newNoteWarning in a paragraph. If the newNote.value.length is less than 9, return newNoteWarning with an initialized message in it.
+
+` if (newNote.value.length < 9) 
+{
+    return (newNoteWarning.value = "Note needs to be more than 10 charactera");
+}`
+
+The reason we are saying `<p v-if="newNoteWarning"> {{newNoteWarning}}</p>` is coz we know newNoteWarning is an empty string by default, and an empty string is `falsy` in JS. So, if its `true` (which is not at the start), then it will be rendered in the paragraph.
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,184 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+
+// MODAL STATE
+// First we declare the state of the modal. Then we use directives to change that state
+// After that, we want to store the state of the user's input, & bind it to our input element
+// Next is to store each note in an array.
+
+const showModal = ref(false);
+
+//const showHeading = ref(true);
+
+const newNote = ref("");
+
+const newNoteWarning = ref("");
+
+const notes = ref([]);
+
+const addNote = () => {
+  //add notes if input characters are > 10
+  if (newNote.value.length < 9) {
+    return (newNoteWarning.value = "Note needs to be more than 10 charactera");
+    //this prevents the form from continuing
+    //we are trmming to get rid of whitespaces in the todo
+  }
+  notes.value.push({
+    id: Math.floor(Math.random() * 1000000),
+    text: newNote.value,
+    date: new Date(),
+    backgroundColor: getRandomColor(),
+  });
+
+  showModal.value = !showModal; //close the modal after adding note
+  newNote.value = ""; //reset the value of newNote
+  newNoteWarning.value = "";
+};
+
+//generate random light color
+function getRandomColor() {
+  return "hsl( " + Math.random() * 360 + ", 100%,  75%)";
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- THIS IS WHERE HTML WILL BE   -->
+  <main>
+    <!-- <button
+      v-bind="showHeading"
+      @click="showHeading = !showHeading"
+      v-if="showHeading"
+    >
+      If TRUE, now you see me!
+    </button> -->
+
+    <div v-if="showModal" class="overlay">
+      <div class="modal">
+        {{ newNote }}
+        <!--because newNote is 2- way binded, we dont need to use
+        computed properties to keep track of letter input coz vue is already
+        doing that for us. -->
+        <textarea
+          v-model.trim="newNote"
+          name="notes"
+          id="notes"
+          cols="30"
+          rows="10"
+        ></textarea>
+        <!-- we can also trim on the v-model -->
+        <p v-if="newNoteWarning">{{ newNoteWarning }}</p>
+        <button @click="addNote">Add Note</button>
+        <button class="close" @click="showModal = !showModal">Close</button>
+      </div>
+    </div>
+
+    <div class="container">
+      <header>
+        <!-- {{ notes }} -->
+        <h1>Notes {{ showModal }}</h1>
+        <button @click="showModal = true">+</button>
+      </header>
+
+      <div class="cards-container">
+        <div
+          v-for="note in notes"
+          :key="note.id"
+          class="card"
+          :style="{ backgroundColor: note.backgroundColor }"
+        >
+          <p class="main-text">
+            {{ note.text }}
+          </p>
+          <p class="date">{{ note.date.toLocaleDateString("en-US") }}</p>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+main {
+  height: 100vh;
+  width: 100vh;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.container {
+  max-width: 1000px;
+  padding: 10px;
+  margin: 0 auto;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.cards-container {
+  display: flex;
+  /*flex-direction: row;*/
+  flex-wrap: wrap;
+}
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 10;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+h1 {
+  font-weight: bold;
+  margin-bottom: 25px;
+  font-size: 75px;
+}
+header button {
+  border: none;
+  padding: 10px;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  background-color: rgb(21, 20, 20);
+  border-radius: 100%;
+  color: white;
+  font-size: 20px;
+}
+
+/** CARD  **/
+.card {
+  width: 225px;
+  height: 225px;
+  background-color: rgb(237, 182, 44);
+  padding: 10px;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-right: 20px;
+  margin-bottom: 20px;
+}
+.modal {
+  width: 150px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+.modal button {
+  padding: 10px 20px;
+  margin-top: 15px;
+  font-size: 20px;
+  width: 100%;
+  border: none;
+  color: white;
+  cursor: pointer;
+}
+.close {
+  background-color: red;
+  margin-top: 15px;
+}
+.modal p {
+  color: red;
 }
 </style>
